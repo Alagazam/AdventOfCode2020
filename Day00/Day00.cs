772db246eae8c9cd43b1de97d97799e02be8687d
David Darj <zid@alagazam.net>
2020-12-06 22:43:06 +0100

    Added template as Day00

---
+++ Day00/Day00.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AoC2020
+{
+    public class Day00
+    {
+        public static Int64 Day00a(string[] input)
+        {
+            return 0;
+        }
+
+        public static Int64 Day00b(string[] input)
+        {
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("Day00.txt");
+            Console.WriteLine("Day00a : {0}", Day00a(lines));
+            Console.WriteLine("Day00b : {0}", Day00b(lines));
+        }
+    }
+}
---
+++ Day00/Day00Test.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace AoC2020
+{
+    public class Day00Test
+    {
+        string input =
+@"";
+
+        [Fact]
+        public void Day00a()
+        {
+            var lines = input.Split(Environment.NewLine);
+
+            Assert.Equal(0, Day00.Day00a(lines));
+        }
+
+
+        [Fact]
+        public void Day00b()
+        {
+            var lines = input.Split(Environment.NewLine);
+
+            Assert.Equal(0, Day00.Day00b(lines));
+        }
+    }
+
+}
