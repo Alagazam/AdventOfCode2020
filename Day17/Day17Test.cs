8b8951c45b9ce7ec99ba635f1d1904b7393e6954
David Darj <zid@alagazam.net>
2020-12-18 11:12:24 +0100

    Day17 part 1 done

---
+++ Day17/Day17Test.cs
@@ -0,0 +1,40 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AoC2020
+{
+    public class Day17Test
+    {
+        string input =
+@".#.
+..#
+###
+";
+
+        [Fact]
+        public void Day17a()
+        {
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            Assert.Equal(112, Day17.Day17a(lines));
+        }
+
+
+        [Fact]
+        public void Day17b()
+        {
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            Assert.Equal(0, Day17.Day17b(lines));
+        }
+
+        public Day17Test(ITestOutputHelper output)
+        {
+            var converter = new AoCUtils.Converter(output);
+            Console.SetOut(converter);
+        }
+
+    }
+
+}
---
+++ getinput.cmd
@@ -0,0 +1,5 @@
+SET DAY=0%1
+SET DAY=%DAY:~-2%
+echo %DAY%
+
+curl --output Day%DAY%\Day%DAY%.txt https://adventofcode.com/2020/day/%1/input --cookie "session=%AoCcookie%"
